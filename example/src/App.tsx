56cff836db88a41fd61b4ff1615994fca6a6467b
chanduka <chandukaketha@gmail.com>
2024-06-15 23:43:19 +0100

    chore: conf update

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,40 @@
 import * as React from 'react';
 
 import { Alert, Button, StyleSheet, View } from 'react-native';
-import WalletPass from 'react-native-pass-manager';
 import { mockData } from './mock';
+import PassManager from 'react-native-pass-manager';
 
 export default function App() {
+  const [walletAvailable, setWalletAvailable] = React.useState(false);
+
+  const checkWallet = async () => {
+    try {
+      const isAvailable = await PassManager.isWalletAvailable();
+      setWalletAvailable(isAvailable);
+    } catch (error) {
+      console.log('Error checking if wallet is available:', error);
+    }
+  };
+
+  React.useEffect(() => {
+    checkWallet();
+  }, []);
+
   const savePassToWallet = async () => {
     try {
       const base64Encoded = mockData!.base64data;
-      await WalletPass.saveToWallet(base64Encoded);
+      await PassManager.saveToWallet(base64Encoded);
       console.log('Pass saved to wallet');
     } catch (error) {
       console.log('Error saving pass to wallet:', error);
-
       Alert.alert('Error', 'Failed to save pass to wallet');
     }
   };
   return (
     <View style={styles.container}>
-      <Button title="Save Pass to Wallet" onPress={savePassToWallet} />
+      {walletAvailable && (
+        <Button title="Save Pass to Wallet" onPress={savePassToWallet} />
+      )}
     </View>
   );
 }
