f0a5668e4a51f405b26c462f7adf25be32a0329a
chanduka <chandukaketha@gmail.com>
2024-08-30 16:37:53 +0100

    feat: ios removed view controllers

--- ios/AppleWallet.swift
+++ ios/AppleWallet.swift
@@ -2,9 +2,8 @@ import PassKit
 
 @objc(AppleWallet)
 class AppleWallet: NSObject, PKAddPassesViewControllerDelegate {
-    var resolve: RCTPromiseResolveBlock?
-    var reject: RCTPromiseRejectBlock?
-    var pass: PKPass?
+    private var resolve: RCTPromiseResolveBlock?
+    private var reject: RCTPromiseRejectBlock?
 
     @objc(isWalletAvailable:rejecter:)
     func isWalletAvailable(resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
@@ -15,85 +14,57 @@ class AppleWallet: NSObject, PKAddPassesViewControllerDelegate {
         }
     }
 
-    @objc(saveToAppleWallet:resolver:rejecter:)
-    func saveToAppleWallet(base64Encoded: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
-        guard let passData = Data(base64Encoded: base64Encoded) else {
-            reject("ERROR", "Failed to decode base64 string", nil)
+    @objc(addPass:resolver:rejecter:)
+    func addPass(_ base64EncodedPass: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+        self.resolve = resolve
+        self.reject = reject
+
+        guard let passData = Data(base64Encoded: base64EncodedPass) else {
+            reject("ERROR", "Invalid base64 string", nil)
             return
         }
+
         do {
             let pass = try PKPass(data: passData)
-            self.pass = pass
-            self.resolve = resolve
-            self.reject = reject
-
-            DispatchQueue.main.async {
-    let scenes = UIApplication.shared.connectedScenes
-    print("Connected scenes: \(scenes)")
-    
-    if let scene = scenes.first as? UIWindowScene {
-        print("Found UIWindowScene: \(scene)")
-        
-        if let window = scene.windows.first {
-            print("Found window: \(window)")
-            
-            if let rootViewController = window.rootViewController {
-                print("Found rootViewController: \(rootViewController)")
-                
-                let addPassesViewController = PKAddPassesViewController(pass: pass)
-                addPassesViewController?.delegate = self
-                rootViewController.present(addPassesViewController ?? UIViewController(), animated: true, completion: nil)
-            } else {
-                print("No rootViewController found")
-                reject("ERROR", "No rootViewController found", nil)
-            }
-        } else {
-            print("No window found")
-            reject("ERROR", "No window found", nil)
-        }
-    } else {
-        print("No UIWindowScene found")
-        reject("ERROR", "No UIWindowScene found", nil)
-    }
-}
+            showPassInWallet(pass: pass)
         } catch {
-            reject("ERROR", "in line 41", error)
+            reject("ERROR", "Failed to create PKPass", nil)
         }
     }
 
-    // PKAddPassesViewControllerDelegate method
+    @objc(saveToAppleWallet:resolver:rejecter:)
+    func saveToAppleWallet(_ base64EncodedPass: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+        addPass(base64EncodedPass, resolve: resolve, reject: reject)
+    }
+
+    private func showPassInWallet(pass: PKPass) {
+        if let rootViewController = UIApplication.shared.windows.first?.rootViewController {
+            let addPassesViewController = PKAddPassesViewController(pass: pass)
+            addPassesViewController?.delegate = self
+            rootViewController.present(addPassesViewController ?? UIViewController(), animated: true, completion: nil)
+        } else {
+            reject?("ERROR", "No rootViewController found", nil)
+        }
+    }
+
     func addPassesViewControllerDidFinish(_ controller: PKAddPassesViewController) {
         controller.dismiss(animated: true) {
-            let passLibrary = PKPassLibrary()
-            if let pass = self.pass {
-                if passLibrary.containsPass(pass) {
-                    self.resolve?(["success": true, "status": "didAddPasses"])
-                    self.showPassInWallet(pass)
-                } else {
-                    self.resolve?(["success": false, "status": "didAddPasses"])
-                }
-            } else {
-                self.reject?("ERROR", "in line 57", nil)
-            }
+            let result = ["success": true, "status": "didAddPasses"]
+            self.resolve?(result)
+            self.navigateToWallet()
         }
     }
 
-    private func showPassInWallet(_ pass: PKPass) {
-        if let passURL = pass.passURL {
-            DispatchQueue.main.async {
-                UIApplication.shared.open(passURL, options: [:], completionHandler: { success in
-                    if success {
-                        // Optionally, you can add a delay to ensure the Wallet app is opened before returning the status
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                            self.resolve?(["success": true, "status": "didAddPasses"])
-                        }
-                    } else {
-                        self.reject?("ERROR", "Failed to open Wallet app", nil)
-                    }
-                })
-            }
-        } else {
-            self.reject?("ERROR", "Pass URL not found", nil)
+    func addPassesViewControllerDidCancel(_ controller: PKAddPassesViewController) {
+        controller.dismiss(animated: true) {
+            let result = ["success": false, "status": "didAddPasses"]
+            self.resolve?(result)
+        }
+    }
+
+    private func navigateToWallet() {
+        if let url = URL(string: "shoebox://"), UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
 }
